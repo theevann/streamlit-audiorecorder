21154a3696718f5c7c075799c98756d5ed46f67f
Evann Courdier <evann.courdier@epfl.ch>
2024-07-05 12:49:30 +0200

    Simplify whisper example

--- example/example_whisper.py
+++ example/example_whisper.py
@@ -5,8 +5,12 @@ import whisper
 from audiorecorder import audiorecorder
 from scipy import signal
 
-st.title("Audio Recorder")
+st.title("Audio Recorder with Whisper Model")
 audio = audiorecorder('', '')
+
+@st.cache_resource
+def load_model(size="small"):
+    return whisper.load_model(size, in_memory=True)
 
 if len(audio) > 0:
     # To play audio in frontend:
@@ -16,23 +20,15 @@ if len(audio) > 0:
     st.write(f"Frame rate: {audio.frame_rate}, Frame width: {audio.frame_width}, Duration: {audio.duration_seconds} seconds")
     
     # Prepare the audio data for Whisper model
+    # Skipping the WAV header (44 bytes) when reading audio data from buffer
     audio_buffer = io.BytesIO()
-    audio.export(audio_buffer, format="wav")
-    audio_buffer.seek(0)  # Reset buffer position to the start
-    audio_buffer.read(44)  # Skip the WAV header (44 bytes)
-
-    # Convert audio bytes to a NumPy array
-    audio_array = np.frombuffer(audio_buffer.read(), dtype=np.int16)
-    audio_array = audio_array.astype(np.float32) / 32768.0  # Normalize to [-1.0, 1.0]
-
-    # Resample audio to 16000 Hz if necessary
-    if audio.frame_rate != 16000:
-        num_samples = round(len(audio_array) * 16000 / audio.frame_rate)
-        audio_array = signal.resample(audio_array, num_samples)
-
+    audio.export(audio_buffer, format="wav", parameters=["-ar", str(16000)])
+    audio_array = np.frombuffer(audio_buffer.getvalue()[44:], dtype=np.int16).astype(np.float32) / 32768.0
+    
     # Load the Whisper model and transcribe the audio
-    model = whisper.load_model("small")
-    result = model.transcribe(audio_array)
+    with st.spinner("Transcribing..."):
+        model = load_model()
+        result = model.transcribe(audio_array, fp16=False)
 
     # Display the transcription
     st.write(f"Transcription: {result['text']}")
